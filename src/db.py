7ecd9b74763ecdae1c67d324ec9ca1a7bcd07ec0
Arthur Sousa <speeedofc@gmail.com>
2021-06-28 18:33:17 -0300

    big changes!
    Those are big changes: new organization on the prompts, new functions and a new database for done todos.

--- src/db.py
+++ src/db.py
@@ -2,6 +2,38 @@ import sqlite3
 import os
 from datetime import *
 import locale
+import shutil
+
+# date and welcome
+locale.setlocale(locale.LC_ALL, '')
+today = datetime.today().strftime('%d-%m-%B').split('-')
+print(f'Boas vindas ao terminal todo! Hoje é dia {today[0]} de {today[2]}!')
+
+## useful functions
+# convert tuple to string
+def toString(tuple):
+    if type(tuple) == str:
+        return tuple.replace('(', '').replace(')', '').replace(',', '').replace("'", "")
+    else: 
+        return str(tuple).replace('(', '').replace(')', '').replace(',', '').replace("'", "")
+
+# convert date words to numbers
+def toDate(date):
+    td = [['today', 'hoje', 'hj'], ['amanha', 'amanhã', 'amnh', 'tomorrow'], ['semana que vem']]
+    if date in td[0]:
+        date = today[0]
+    elif date in td[1]:
+        date = str(int(today[0]) + 1)
+    elif date in td[2]:
+        date = str(int(today[0]) + 7)
+
+    return date
+
+def toNotDate(date):
+    if date == today[0]:                    return 'hoje'
+    elif date == str(int(today[0]) + 1):    return 'amanhã'
+    elif date == str(int(today[0]) + 7):    return 'semana que vem'
+    else:                                   return date
 
 # database creation
 cur = os.path.abspath(os.getcwd())
@@ -9,6 +41,7 @@ if not os.path.isfile(f'{cur}/db.sqlite'):
     open(f'{cur}/db.sqlite', 'x')
 else: pass
 
+# database setup
 database = sqlite3.connect('db.sqlite')
 db = database.cursor()
 db.execute("""CREATE TABLE IF NOT EXISTS dates (
@@ -20,33 +53,52 @@ db.execute("""CREATE TABLE IF NOT EXISTS items (
     item TEXT
 );""")
 
-# date and items for today 
-locale.setlocale(locale.LC_ALL, '')
-today = datetime.today().strftime('%d-%m-%B').split('-')
-print(f'Boas vindas ao terminal todo! Hoje é dia {today[0]} de {today[2]}!')
+# database for done items
+if not os.path.isfile(f'{cur}/itemsdone.sqlite'):
+    open(f'{cur}/itemsdone.sqlite', 'x')
+else: pass
 
+_database = sqlite3.connect('itemsdone.sqlite')
+_db = _database.cursor()
+_db.execute('CREATE TABLE IF NOT EXISTS done (date TEXT, item TEXT)')
+
+# function for adding items do done db
+def doneDatabase(date, item):
+    if date and item:
+        _db.execute('INSERT INTO done VALUES (?, ?)', (date.replace("'", ''), str(item),))
+        _database.commit()
+    else: return 0
+
+# init function
 def init():
+    # delete todos from dates before today
     db.execute(f"SELECT dates_id FROM dates WHERE date < ?", (today[0],))
-    date1_id = str(db.fetchone()).replace('(', '').replace(')', '').replace(',', '')
+    date1_id = toString(db.fetchone())
     db.execute("DELETE FROM items WHERE dates_id = ?", (date1_id,))
     db.execute('DELETE FROM dates WHERE date < ?', (today[0],))
+    database.commit()
 
+    # print all todos for today
     db.execute(f"SELECT dates_id FROM dates WHERE date = ?", (today[0],))
-    date2_id = str(db.fetchone()).replace('(', '').replace(')', '').replace(',', '')
+    date2_id = toString(db.fetchone())
     db.execute('SELECT item FROM items WHERE dates_id = ?', (date2_id,))
     i = db.fetchall()
     
     if i:
         print('Items para hoje: ')
         for row in range(len(i)):
-            sla = str(i[row]).replace('(', '').replace(')', '').replace(',', '').replace("'", "")
+            sla = toString(i[row])
             print(f"- {sla}")
+
     else: print('Sem itens para hoje!')
 
 init()
 
-# functions
-def addItem(date, item):
+## functions
+# add item to database
+def addItem(dater, item):
+    date = toDate(dater)
+
     db.execute(f'SELECT * FROM dates WHERE date = ?', (date,))
     data = db.fetchall()
     
@@ -57,7 +109,7 @@ def addItem(date, item):
         date_id = db.fetchall()[0][0]
 
         db.execute(f"INSERT INTO items VALUES (?, ?)", (str(date_id), str(item),))
-        print(f"Criada data {date}")
+        print(f"Item adicionado, criada data {date}")
     else:
         db.execute(f"SELECT dates_id FROM dates WHERE date = ?", (date,))
         date_id = db.fetchall()[0][0]
@@ -67,18 +119,87 @@ def addItem(date, item):
 
     database.commit()
 
+# done item - basically remove item from database
 def doneItem(item):
-    def findItem():
-        db.execute(f'SELECT * FROM items WHERE item = ?', (item,))
-        data = db.fetchall()[0]
-        if data: return data
-        else: return False
-    
-    if findItem():
+    db.execute(f'SELECT * FROM items WHERE item = ?', (item,))
+    data = db.fetchall()[0]
+    if data: 
         print(f'Item encontrado! Excluindo item...')
+        db.execute('SELECT dates_id FROM items WHERE item = ?', (item,))
+        date_id = db.fetchone()[0]
+        db.execute('SELECT date FROM dates WHERE dates_id = ?', (date_id,))
+        dat = toString(db.fetchone())
+        doneDatabase(dat, item)
+
         db.execute('DELETE FROM items WHERE item = ?', (item,))
         print('Item excluído!')
     else: 
         print('Item não encontrado...')
-    
+        
     database.commit()
+
+# show all todos from specific date
+def showItems(dater):
+
+    date = toDate(dater)
+    db.execute('SELECT dates_id FROM dates WHERE date = ?', (date,))
+    row = db.fetchall()
+    if row: 
+        print(f'Itens para {toNotDate(date)}:\n')
+        list = toString(row[0])
+        db.execute('SELECT item FROM items WHERE dates_id = ?', (list,))
+        item = db.fetchall()
+        for i in range(len(item)):
+            _item = toString(item[i])
+            print(f"- {_item}")
+    else: print('Data não existente')
+
+# show all todos and their date
+def showAll():
+    db.execute('SELECT dates_id FROM dates')
+    d = db.fetchall()
+    if d:
+        for i in range(len(d)):
+            list = d[i][0]
+            db.execute('SELECT date FROM dates WHERE dates_id = ?', str(list,))
+            date = toString(db.fetchone())
+            print(f"\nTodos do dia {toNotDate(date)}:")
+
+            db.execute('SELECT item FROM items WHERE dates_id = ?', str(list,))
+            it = db.fetchall()
+            for row in range(len(it)):
+                list = toString(it[row])
+                print(f"- {list}")
+    else:
+        print('Ainda não há nenhum item!')
+
+# show all done todos
+def showDone():
+    _db.execute('SELECT date FROM done')
+    daate = _db.fetchall()
+
+    def parseDuplicated():
+        lista = []
+        for i in range(len(daate)):
+            lista.append(toString(daate[i]))
+
+        return list(dict.fromkeys(lista))
+    
+    _date = parseDuplicated()
+
+    for i in range(len(_date)):
+        _db.execute("SELECT item FROM done WHERE date = ?", (str(_date[i]),))
+        _item = _db.fetchall()
+
+        print(f'\nItems feitos de {toNotDate(_date[i])}')
+        for row in range(len(_item)):
+            itt = toString(_item[row])
+
+            print(f"- {itt}")
+
+# reset entire aplication to default
+def resetDefault():
+    os.remove(f'{cur}/db.sqlite')
+    os.remove(f'{cur}/itemsdone.sqlite')
+    shutil.rmtree(f'{cur}/__pycache__', ignore_errors=True)
+    
--- src/main.py
+++ src/main.py
@@ -1,25 +1,75 @@
-from db import addItem, doneItem, today
+from db import addItem, doneItem, showItems, showAll, showDone, resetDefault
 
+# main function
 def main():
-    command = input('O que deseja fazer? Para saber dos comandos, use help:\n').split()
+    yes = ['sim', 'si', 'sisi', 'yes', 'yeah', 'yep', 'aye']
+    def anyMore():
+        answer = input('\nAlgo mais? \n> ')
+        if answer in yes: main()
+        else: exit()
+
+
+    command = input('\nO que deseja fazer? Para saber dos comandos, use help:\n> ').split()
+    
+    # help command
     if command[0] == 'help':
-        print('todo data item\ndone item\n')
+        print('''
+        use "todo" + data da sua atividade + item a fazer!\n
+        use "done" + item que você finalizou!\n
+        "show" + data para ver TODOs de tal data!\n
+        "showall" para ver todos os TODOs!\n
+        "showdone" para ver todos os TODOs finalizados!\n
+        "reset" para resetar a aplicação!
+        ''')
         main()
+
+    # add todo command
     elif command[0] == 'todo':
         item = ''
         for i in range(2, len(command)):
             item += f"{command[i]} "
 
         addItem(command[1], item)
+        anyMore()
+
+    # done todo command
     elif command[0] == 'done':
         item = ''
         for i in range(1, len(command)):
             item += f"{command[i]} "
 
         doneItem(item)
+        anyMore()
+
+    # show todo from specific date command
+    elif command[0] == 'show':
+        showItems(command[1])
+        anyMore()
+
+    # show all todos command
+    elif command[0] == 'showall':
+        showAll()
+        anyMore()
+
+    # show all done todos
+    elif command[0] == 'showdone':
+        showDone()
+        anyMore()
+
+    # reset to default command
+    elif command[0] == 'reset':
+        st = 'Tem certeza? Digite "sim eu tenho" para excluir as databases: \n> '
+        if input(st) == 'sim eu tenho':
+            resetDefault()
+            print('Aplicação resetada!')
+            exit()
+        else: 
+            print('Ok, não excluirei!\n')
+            anyMore()
+
+    # command not recognized
     else:
-        yes = ['sim', 'si', 'sisi', 'yes', 'yeah', 'yep', 'aye']
-        if input('Não entendi. Quer tentar novamente? Use todo ou done!\n') in yes: main()
+        if input('Não entendi. Quer tentar novamente?\n> ') in yes: main()
         else: exit()
     
 main()
